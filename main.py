17c1269da84585c42dcd240bc366b927e9f97dd5
riguima <richard.alexsander.guima@gmail.com>
2023-09-06 12:39:49 -0300

    fix: add app.run

--- main.py
+++ main.py
@@ -9,29 +9,30 @@ from pyrogram.types import Message
 from telegram_chatbot.use_cases.bing import BingChatbot
 
 
-def create_app() -> Client:
-    load_dotenv()
-    app = Client(
-        os.environ['BOT_NAME'],
-        api_id=os.environ['API_ID'],
-        api_hash=os.environ['API_HASH'],
-    )
+load_dotenv()
+app = Client(
+    os.environ['BOT_NAME'],
+    api_id=os.environ['API_ID'],
+    api_hash=os.environ['API_HASH'],
+)
 
-    @app.on_message()
-    async def ask(client: Client, message: Message) -> None:
-        cookies = json.load(open('cookies.json', encoding='utf-8'))
-        bot = await Chatbot.create(cookies=cookies)
-        chatbot = BingChatbot(bot)
-        searching = await message.reply('Pesquisando...')
-        if 'quero imagens' in message.text.lower():
-            await chatbot.generate_images(message.text)
-            for filename in os.listdir('images'):
-                await message.reply_photo(f'images/{filename}')
-                os.remove(f'images/{filename}')
-        else:
-            response = await chatbot.ask(message.text)
-            await message.reply(response)
-        await searching.delete()
-        await bot.close()
+@app.on_message()
+async def ask(_, message: Message) -> None:
+    cookies = json.load(open('cookies.json', encoding='utf-8'))
+    bot = await Chatbot.create(cookies=cookies)
+    chatbot = BingChatbot(bot)
+    searching = await message.reply('Pesquisando...')
+    if 'quero imagens' in message.text.lower():
+        await chatbot.generate_images(message.text)
+        for filename in os.listdir('images'):
+            await message.reply_photo(f'images/{filename}')
+            os.remove(f'images/{filename}')
+    else:
+        response = await chatbot.ask(message.text)
+        await message.reply(response)
+    await searching.delete()
+    await bot.close()
 
-    return app
+
+if __name__ == '__main__':
+    app.run()
